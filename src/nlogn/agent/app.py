673c57ead8bf0b7dc8f7d87f10c25076704d47f4
Mher Kazandjian <mherkazandjian@gmail.com>
2022-01-22 23:44:34 +0100

    add the timestamping snippet

--- src/nlogn/agent/app.py
+++ src/nlogn/agent/app.py
@@ -11,7 +11,9 @@ from requests.auth import HTTPBasicAuth
 import schedule
 from schedule import repeat
 import psutil
-
+import datetime
+import dateutil
+from dateutil.tz import tzutc
 from nlogn.agent.argparser import parse_args
 
 
@@ -36,40 +38,21 @@ if __name__ == '__main__':
         'cert': args.trusted_certificate
     }
 
-
-    def job1(stats=None, connection=None, *args, **kwargs):
-        # if the previous job has completed (if applicable)
-        # then collect the data by e.g executing the command
-        # otherwise skip and wait for next execution
-        #  - if a stuck sitation is detected, log an alarm
-        #
-        stats['a'] += 1
-        print(f'job1 {stats["a"]}')
-        # job1_count += 1
-        # print(job1_count)
-        data = {
-            'hostname': 'my_host',
-            't_start': 'YYYY:MM:DD-HH:mm:ss',  # utc time
-            't_end': 'YYYY:MM:DD-HH:mm:ss',  # utc time
-            'fs_mount': '/',
-            'bytes_total': 11111111,
-            'types_free': 22222222
-        }
-
-    stats_1 = {}
-    stats_1['a'] = 0
-    schedule.every(2).seconds.do(job1, stats=stats_1, connection=conn)
-
-
     def job2(stats=None, connection=None, *args, **kwargs):
         stats['a'] += 1
         pct_cpu = psutil.cpu_percent()
         print(f'job2 {stats["a"]}')
 
+        t = datetime.datetime.utcnow()
+        timestamp = t.strftime('%Y-%m-%dT%H:%M:%S')
+        print(timestamp)
+
         data = {
+            'job_name': 'job2',
+            'pipeline': 'pipelinex',
             'hostname': 'my_host',
-            't_start': 'YYYY:MM:DD-HH:mm:ss',  # utc time
-            't_end': 'YYYY:MM:DD-HH:mm:ss',  # utc time
+            't_start': timestamp,
+            't_end': timestamp,
             'pct_cpu': pct_cpu
         }
 
