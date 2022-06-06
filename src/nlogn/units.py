a910c92cfea31034eca629a9d2464f8c3e8d2517
Mher Kazandjian <mherkazandjian@gmail.com>
2022-06-06 14:28:55 +0200

    add the Bytes (B) unit and rename the units registry to u

--- src/nlogn/units.py
+++ src/nlogn/units.py
@@ -8,9 +8,10 @@ types_map = {
     'date': str
 }
 
-_ureg = pint.UnitRegistry()
+u = pint.UnitRegistry()
 bytes_units_converter = pint.UnitRegistry(filename=None)
-bytes_units_converter.define('KB = []')
+bytes_units_converter.define('B = []')
+bytes_units_converter.define('KB = 1024 B')
 bytes_units_converter.define('MB = 1024 KB')
 bytes_units_converter.define('GB = 1048576 KB')
 
