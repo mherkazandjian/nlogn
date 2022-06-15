afa8cd0863a35a520986d13e0370da1f2c318c90
Mher Kazandjian <mherkazandjian@gmail.com>
2022-06-15 02:25:25 +0200

    update the logger and modularize it and add file handler support for it

--- src/nlogn/loggers/logger.py
+++ src/nlogn/loggers/logger.py
@@ -2,48 +2,23 @@ import os
 import sys
 import logging
 
+
 BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
 # The background is set with 40 plus the number of the color, and the
 # foreground with 30
 
-# These are the sequences need to get colored ouput
-RESET_SEQ = "\033[0m"
-COLOR_SEQ = "\033[1;%dm"
-BOLD_SEQ = "\033[1m"
+
+logger_name = 'nlogn'
+logger_level_env_var = 'NLOGN_LOG_LEVEL'
 
 
-def formatter_message(message, use_color = True):
-    if use_color:
-        message = message.replace("$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
-    else:
-        message = message.replace("$RESET", "").replace("$BOLD", "")
-    return message
-
-COLORS = {
+LOG_COLORS = {
     'WARNING': YELLOW,
     'INFO': WHITE,
     'DEBUG': BLUE,
-    'CRITICAL': YELLOW,
+    'CRITICAL': MAGENTA,
     'ERROR': RED
 }
-
-
-class ColoredFormatter(logging.Formatter):
-    def __init__(self, msg, use_color=True):
-        logging.Formatter.__init__(self, msg)
-        self.use_color = use_color
-
-    def format(self, record):
-        levelname = record.levelname
-        if self.use_color and levelname in COLORS:
-            levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
-            record.levelname = levelname_color
-        return logging.Formatter.format(self, record)
-
-
-# create logger
-logger = logging.getLogger('nlogn')
-logger.log_depth = 0
 
 log_levels = {
     'notest': logging.NOTSET,
@@ -54,43 +29,113 @@ log_levels = {
     'critical': logging.CRITICAL
 }
 
+# These are the sequences need to get colored output
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
 
-if not len(logger.handlers):
 
-    _log_level = os.environ.get('NLOGN_LOG_LEVEL', 'info')
-    log_level = log_levels[_log_level]
+def formatter_message(message, use_color=True):
+    if use_color:
+        message = message.replace("$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
 
-    logger.setLevel(log_level)
 
-    # create console handler and set level to debug
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(log_level)
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color=True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
 
-    # create formatter
-    formatter = logging.Formatter(
-        ('[%(asctime)s %(funcName)s() %(filename)s'
-         ':%(lineno)-7d][%(levelname)-8s] %(message)s'))
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in LOG_COLORS:
+            levelname_color = COLOR_SEQ % (30 + LOG_COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
 
-    # just like print()
-    # formatter = logging.Formatter('%(message)s')
 
-    # a color formatter
-    FORMAT = ("[%(asctime)s %(levelname)-18s "
-              "$BOLD%(filename)s{%(lineno)-5d}$RESET:%(funcName)s()] "
-              "%(message)s")
-    COLOR_FORMAT = formatter_message(FORMAT, True)
-    formatter = ColoredFormatter(COLOR_FORMAT)
+class CustomFileHandler(logging.FileHandler):
+    def __init__(self, *args, **kwargs):
+        super(CustomFileHandler, self).__init__(*args, **kwargs)
 
-    # add formatter to ch
-    ch.setFormatter(formatter)
 
-    # add ch to logger
-    logger.addHandler(ch)
+def create_stream_handler(log_level):
+    """
+    Create console handler and set level to debug
 
-    # DEV
+    :param log_level: the logger log level
+    :return: the stream handler
+    """
+    channel = logging.StreamHandler(sys.stdout)
+    channel.setLevel(log_level)
+
+    fmt = ("[%(asctime)s %(levelname)-18s "
+           "$BOLD%(filename)s{%(lineno)-5d}$RESET:%(funcName)s()] "
+           "%(message)s")
+
+    formatter = formatter_message(fmt)
+    color_formatter = ColoredFormatter(formatter)
+
+    channel.setFormatter(color_formatter)
+
+    return channel
+
+
+def create_file_handler(log_level: str = 'debug', fpath: str = 'debug.log'):
+    """
+    Create console handler and set level to debug
+
+    :param log_level: the logger log level
+    :param fpath: the path to the log file
+    :return: the stream handler
+    """
+    channel = logging.FileHandler(os.path.join(fpath), "w")
+    channel.setLevel(log_levels[log_level])
+    fmt = ("[%(asctime)s %(levelname)-18s "
+           "%(filename)s{%(lineno)-5d}:%(funcName)s()] "
+           "%(message)s")
+
+    formatter = logging.Formatter(fmt)
+    channel.setFormatter(formatter)
+
+    return channel
+
+
+def test_all_log_levels(logger):
     logger.info('test info message')
     logger.debug('test debug message')
     logger.warning('test warning message')
     logger.error('test error message')
     logger.critical('test critical message')
+
+
+def create_logger():
+
+    # create the logger instance
+    logger = logging.getLogger(logger_name)
+    logger.log_depth = 0
+
+    _log_level = os.environ.get(logger_level_env_var, 'info')
+    log_level = log_levels[_log_level]
+    logger.setLevel(log_level)
+
+    # add terminal output channel to logger
+    stream_handler = create_stream_handler(log_level)
+    logger.addHandler(stream_handler)
+
+    # add the file output channel to the logger
+    #file_handler = create_file_handler(log_level)
+    #logger.addHandler(file_handler)
+
+    # DEV
+    test_all_log_levels(logger)
     # END DEV
+
+    return logger
+
+
+logger = create_logger()
+
+
